e62617e8af04d63460e43657ba635e31dbd92165
anabh05 <anabh05@gmail.com>
2024-07-09 17:49:17 +0200

    redirigir

--- app/Controllers/Landing.php
+++ app/Controllers/Landing.php
@@ -6,44 +6,41 @@ class Landing extends BaseController
 {
     public function index(): string
     {
-        // Verificar si el usuario está autenticado
+
         if (!session()->has('logged_in') || !session('logged_in')) {
-            return redirect()->to('/login'); // Redirigir a la página de inicio de sesión si no está autenticado
+            return redirect()->to('/login');
         }
 
-        // Obtener el ID del video de la sesión o establecer uno predeterminado
+        // ID DEL VIDEO
         $video_id = session()->get('video_id', 'UJ0Z8JBFIYw');
 
-        // Cargar la vista de la página de aterrizaje con el ID del video
         return view('landing', ['video_id' => $video_id]);
     }
 
-    // Función para limpiar y validar el URL del video de YouTube
+    // VALIDAR URL VIDEO
     private function validateYouTubeURL($url)
     {
         if (empty($url)) {
             return "";
         }
 
-        // Expresión regular para validar un enlace de YouTube
+        // VALIDAR ENLACE
         $pattern = '/^(https:\/\/www\.youtube\.com\/embed\/)([\w-]{11})(.*)$/';
         preg_match($pattern, $url, $matches);
 
-        // Si el enlace es válido, devolver el ID del video; de lo contrario, devolver una cadena vacía
+
         return isset($matches[2]) ? $matches[2] : "";
     }
 
-    // Procesamiento del formulario de actualización del video
+    // VIDEO
     public function updateVideo()
     {
         if ($this->request->getMethod() == "post" && $this->request->getPost('update_video')) {
-            // Obtener el nuevo enlace del video desde el formulario
+
             $new_video_url = $this->request->getPost('video_url');
 
-            // Validar y limpiar el nuevo enlace del video
             $video_id = $this->validateYouTubeURL($new_video_url);
 
-            // Actualizar el enlace del video en la sesión para mostrarlo dinámicamente
             session()->set('video_id', $video_id);
         }
 
@@ -51,18 +48,17 @@ class Landing extends BaseController
         return redirect()->to('/landing');
     }
 
-    // Procesamiento del formulario de cerrar sesión
+    // CERRAR SESION
     public function logout()
     {
         if ($this->request->getMethod() == "post" && $this->request->getPost('logout')) {
-            // Destruir todas las variables de sesión
+
             session()->destroy();
 
-            // Redirigir a la página de inicio de sesión
             return redirect()->to('/login');
         }
 
-        // Redirigir de vuelta a la página principal
-        return redirect()->to('/register');
+
+        return redirect()->to('/');
     }
 }
--- app/Views/home.php
+++ app/Views/home.php
@@ -1,15 +1,28 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <title>Formulario de Registro</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de sesión</title>
+    <link href="<?= base_url('node_modules/bootstrap/dist/css/bootstrap.min.css') ?>" rel="stylesheet">
 </head>
 
 <body>
-    <h1>HOME</h1>
-    <a href="app\Views\register.php">Registro</a>
-    <a href="app\Views\login.php">Login</a>
+    <div class="container-fluid vh-100 d-flex justify-content-center align-items-center">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body text-center">
 
+                <h2>Bienvenido</h2>
+
+                <a href="<?= base_url('register') ?>" class="btn btn-primary">Register</a>
+                <a href="<?= base_url('login') ?>" class="btn btn-primary">Login</a>
+            </div>
+
+        </div>
+    </div>
+
+    <script src="<?= base_url('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js') ?>"></script>
 </body>
 
 </html>
